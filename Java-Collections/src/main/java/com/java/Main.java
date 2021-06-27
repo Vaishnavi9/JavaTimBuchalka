aacedab0a1afb1272924224493dc834793da217e
Vaishnavi9 <vaishnavidhanwade.vd@gmail.com>
2021-06-27 17:34:56 +0530

    map

--- Java-Collections/src/main/java/com/java/Main.java
+++ Java-Collections/src/main/java/com/java/Main.java
@@ -5,11 +5,9 @@
 package com.java;
 
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-public class Main {
+/*public class Main {
 
     public static void main(String[] args) {
 	// write your code here
@@ -28,9 +26,9 @@ public class Main {
         Theatre.Seat minSeat = Collections.min(newList);
         Theatre.Seat maxSeat = Collections.max(newList);
 
-      /*  List<Theatre.Seat> test = new ArrayList<Theatre.Seat>(96+1);
+      *//*  List<Theatre.Seat> test = new ArrayList<Theatre.Seat>(96+1);
         Collections.copy(test,theatre.seats);
-        printList(test);*/
+        printList(test);*//*
 
         System.out.println("Minimum ===> "+minSeat.getSeatNumber()+" "+"Maximum ===> "+maxSeat.getSeatNumber());
 
@@ -43,5 +41,39 @@ public class Main {
             System.out.print(i.getSeatNumber()+"  $"+i.getPrice()+"  ");
         }
         System.out.println();
+    }
+}*/
+/******************************************************************************************************/
+class MapExample{
+    public static void main(String[] args) {
+        Map<String, String> example = new HashMap<>();
+        example.put("Java","I know a bit java");
+        example.put("C","I know a tiny bit of C");
+        example.put("Python","Something new and exciting");
+        example.put("Scala","It does not exist now");
+        //iterateMap(example);
+
+        //example.clear();
+        System.out.println(example.entrySet());
+        System.out.println(example.containsKey("Pfh"));
+        System.out.println(example.replace("J","I"));//return null if key value is not present
+        if(example.containsKey("lskjda")){
+            System.out.println(example.replace("Python","Language for data science"));
+        }
+        else{
+            System.out.println("Not present");
+        }
+
+        System.out.println(example.computeIfPresent("fghkdf",(key,value)-> value+"TBN"));
+
+        example.compute("Scala",(key, value)->value+1000);
+        example.computeIfAbsent("IOISU",(k)-> "this is crazy");
+        iterateMap(example);
+    }
+
+    public static void iterateMap(Map<String, String> example){
+        for (String key: example.keySet()) {
+            System.out.println(key+" :"+ example.get(key));
+        }
     }
 }
